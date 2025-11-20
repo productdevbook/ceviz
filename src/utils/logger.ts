612b0cf9329a3e042f775da2af9d58eb8a7b1d8f
selemondev <selemondev19@gmail.com>
2025-11-20 11:47:19 -0700

    refactor(logger): preserve native consola output

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,13 +1,13 @@
 import consola from 'consola'
 
 export const logger = {
-  log: (...args: string[] | unknown[]) => {
-    return consola.log(args.map(arg => arg).join(' '))
+  log: (message: unknown, ...args: unknown[]) => {
+    return consola.log(message, ...args)
   },
-  warn: (...args: string[] | unknown[]) => {
-    return consola.warn(args.map(arg => arg).join(' '))
+  warn: (message: unknown, ...args: unknown[]) => {
+    return consola.warn(message, ...args)
   },
-  error: (...args: string[] | unknown[]) => {
-    return consola.error(args.map(arg => arg).join(' '))
+  error(message: unknown, ...args: unknown[]) {
+    return consola.error(message, ...args)
   },
 }
